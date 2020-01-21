546c3b36f43ebcf24150fccc50f16ef53735c343
Kang Yu <karuiyu@gmail.com>
2020-01-21 11:25:06 -0800

    Finished onlyLetters function

--- out/source/PalindromeChecker.java
+++ out/source/PalindromeChecker.java
@@ -34,6 +34,7 @@ public boolean palindrome(String word)
 {
   word = noCapitals(word);
   word = noSpaces(word);
+  word = onlyLetters(word);
   if(reverse(word).equals(word)) 
   {
     return true;
@@ -59,6 +60,18 @@ public String noSpaces(String str)
   for(int i = 0; i < str.length(); i++)
   {
     if(str.charAt(i) != ' ')
+    {
+      result += str.substring(i, i + 1);
+    }
+  }
+  return result;
+}
+public String onlyLetters(String str)
+{
+  String result = "";
+  for(int i = 0; i < str.length(); i++)
+  {
+    if(Character.isLetter(str.charAt(i)))
     {
       result += str.substring(i, i + 1);
     }
